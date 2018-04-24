5a30ef4b8bfd7d47686830ef1d387e71a15765c2
GalvenD <dennis.galven@gmail.com>
2018-04-24 08:59:23 +0200

    Initial Commit

---
+++ oneshotchallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  oneshotchallenge
+//
+//  Created by Dennis Galvén on 2018-04-24.
+//  Copyright © 2018 GalvenD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
