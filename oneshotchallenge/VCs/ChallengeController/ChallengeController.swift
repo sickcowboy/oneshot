9e5044734c792c2d5574b702d0f5b0a4ad4dd8ee
GalvenD <dennis.galven@gmail.com>
2018-04-25 09:43:47 +0200

    -moved count down timer to a separate ui view class

--- oneshotchallenge/VCs/ChallengeController/ChallengeController.swift
+++ oneshotchallenge/VCs/ChallengeController/ChallengeController.swift
@@ -19,45 +19,7 @@ class ChallengeController: UIViewController {
         return button
     }()
     
-    lazy var hourLabel: UILabel = {
-        return templateTimeLabel()
-    }()
-    
-    lazy var minuteLabel: UILabel = {
-        return templateTimeLabel()
-    }()
-    
-    lazy var secondsLabel: UILabel = {
-       return templateTimeLabel()
-    }()
-    
-    var hours: Int = 0 {
-        didSet{
-            hourLabel.attributedText = templateAttributedTitle(title: "hours", value: hours)
-        }
-    }
-    
-    var minutes: Int = 0 {
-        didSet{
-            if minutes < 0 {
-                hours -= 1
-                minutes = 59
-            }
-            
-            minuteLabel.attributedText = templateAttributedTitle(title: "minutes", value: minutes)
-        }
-    }
-    
-    var seconds: Int = 0 {
-        didSet{
-            if seconds < 0 {
-                minutes -= 1
-                seconds = 59
-            }
-
-            secondsLabel.attributedText = templateAttributedTitle(title: "seconds", value: seconds)
-        }
-    }
+    let countDownTimer = CountDownTimer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -66,8 +28,6 @@ class ChallengeController: UIViewController {
         view.backgroundColor = Colors.sharedInstance.primaryColor
         
         setUp()
-        
-        timeLeft()
     }
     
     fileprivate func setUp() {
@@ -75,49 +35,11 @@ class ChallengeController: UIViewController {
         takeChallengeButton.constraintLayout(top: nil, leading: nil, trailing: nil, bottom: nil, centerX: view.safeAreaLayoutGuide.centerXAnchor, centerY: view.centerYAnchor,
                                              size: .init(width: 0, height: 100))
         
-        setUpTimerViews()
-    }
-    
-    fileprivate func setUpTimerViews() {
-        view.addSubview(minuteLabel)
-        minuteLabel.constraintLayout(top: nil, leading: nil, trailing: nil, bottom: view.safeAreaLayoutGuide.bottomAnchor, centerX: view.centerXAnchor,
-                                     padding: .init(top: 0, left: 0, bottom: 16, right: 0), size: .init(width: 80, height: 0))
+        view.addSubview(countDownTimer)
+        countDownTimer.constraintLayout(top: nil, leading: view.leadingAnchor, trailing: view.trailingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                                        size: .init(width: 0, height: 80))
         
-        view.addSubview(hourLabel)
-        hourLabel.constraintLayout(top: nil, leading: nil, trailing: minuteLabel.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor,
-                                   padding: .init(top: 0, left: 0, bottom: 16, right: 16), size: .init(width: 80, height: 0))
-        
-        view.addSubview(secondsLabel)
-        secondsLabel.constraintLayout(top: nil, leading: minuteLabel.trailingAnchor, trailing: nil, bottom: view.safeAreaLayoutGuide.bottomAnchor,
-                                      padding: .init(top: 0, left: 16, bottom: 16, right: 0), size: .init(width: 80, height: 0))
-    }
-    
-    fileprivate func templateTimeLabel() -> UILabel {
-        let label = UILabel()
-        label.font = UIFont.boldSystemFont(ofSize: 20)
-        label.textColor = Colors.sharedInstance.primaryTextColor
-        label.numberOfLines = 0
-        label.textAlignment = .center
-        return label
-    }
-    
-    fileprivate func templateAttributedTitle(title: String, value: Int) -> NSMutableAttributedString {
-        var valueString = ""
-        
-        if value < 10 {
-            valueString = "0\(value)"
-        } else {
-            valueString = "\(value)"
-        }
-        
-        let attributedTitle = NSMutableAttributedString(string: title,
-                                                        attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 14),
-                                                        NSAttributedStringKey.foregroundColor: Colors.sharedInstance.secondaryColor])
-        attributedTitle.append(NSAttributedString(string: "\n\(valueString)",
-            attributes: [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 52),
-                         NSAttributedStringKey.foregroundColor: Colors.sharedInstance.primaryTextColor]))
-        
-        return attributedTitle
+        timeLeft()
     }
     
     fileprivate func timeLeft() {
@@ -129,17 +51,11 @@ class ChallengeController: UIViewController {
         
         let components = calendar.dateComponents([.hour, .minute, .second], from: timeNow, to: expireDate)
         
-        hours = components.hour ?? 0
-        minutes = components.minute ?? 0
-        seconds = components.second ?? 0
+        let hours = components.hour ?? 0
+        let minutes = components.minute ?? 0
+        let seconds = components.second ?? 0
         
-        countDown()
-    }
-    
-    fileprivate func countDown() {
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
-            self.seconds -= 1
-        })
+        countDownTimer.startCountDown(hours: hours, minutes: minutes, seconds: seconds)
     }
     
     @objc fileprivate func challengePress() {
---
+++ oneshotchallenge/Views/CountDownTimer.swift
@@ -0,0 +1,110 @@
+//
+//  CountDownTimer.swift
+//  oneshotchallenge
+//
+//  Created by Dennis Galvén on 2018-04-25.
+//  Copyright © 2018 GalvenD. All rights reserved.
+//
+
+import UIKit
+
+class CountDownTimer: UIView {
+    
+    lazy var hourLabel: UILabel = {
+        return templateTimeLabel()
+    }()
+    
+    lazy var minuteLabel: UILabel = {
+        return templateTimeLabel()
+    }()
+    
+    lazy var secondsLabel: UILabel = {
+        return templateTimeLabel()
+    }()
+    
+    var hours: Int = 0 {
+        didSet{
+            hourLabel.attributedText = templateAttributedTitle(title: "hours", value: hours)
+        }
+    }
+    
+    var minutes: Int = 0 {
+        didSet{
+            if minutes < 0 {
+                hours -= 1
+                minutes = 59
+            }
+            
+            minuteLabel.attributedText = templateAttributedTitle(title: "minutes", value: minutes)
+        }
+    }
+    
+    var seconds: Int = 0 {
+        didSet{
+            if seconds < 0 {
+                minutes -= 1
+                seconds = 59
+            }
+            
+            secondsLabel.attributedText = templateAttributedTitle(title: "seconds", value: seconds)
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(minuteLabel)
+        minuteLabel.constraintLayout(top: topAnchor, leading: nil, trailing: nil, bottom: bottomAnchor, centerX: centerXAnchor,
+                                     size: .init(width: 80, height: 0))
+        
+        addSubview(hourLabel)
+        hourLabel.constraintLayout(top: topAnchor, leading: nil, trailing: minuteLabel.leadingAnchor, bottom: bottomAnchor,
+                                   padding: .init(top: 0, left: 0, bottom: 0, right: 16), size: .init(width: 80, height: 0))
+        
+        addSubview(secondsLabel)
+        secondsLabel.constraintLayout(top: topAnchor, leading: minuteLabel.trailingAnchor, trailing: nil, bottom: bottomAnchor,
+                                      padding: .init(top: 0, left: 16, bottom: 0, right: 0), size: .init(width: 80, height: 0))
+    }
+    
+    fileprivate func templateTimeLabel() -> UILabel {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.textColor = Colors.sharedInstance.primaryTextColor
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }
+    
+    fileprivate func templateAttributedTitle(title: String, value: Int) -> NSMutableAttributedString {
+        var valueString = ""
+        
+        if value < 10 {
+            valueString = "0\(value)"
+        } else {
+            valueString = "\(value)"
+        }
+        
+        let attributedTitle = NSMutableAttributedString(string: title,
+                                                        attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 14),
+                                                                     NSAttributedStringKey.foregroundColor: Colors.sharedInstance.secondaryColor])
+        attributedTitle.append(NSAttributedString(string: "\n\(valueString)",
+            attributes: [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 52),
+                         NSAttributedStringKey.foregroundColor: Colors.sharedInstance.primaryTextColor]))
+        
+        return attributedTitle
+    }
+    
+    func startCountDown(hours: Int, minutes: Int, seconds: Int) {
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+        
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (_) in
+            self.minutes -= 1
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
