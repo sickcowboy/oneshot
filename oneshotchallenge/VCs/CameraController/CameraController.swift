427f9598cfd27060b5c7ddad2bf0ec639d88c7f3
GalvenD <dennis.galven@gmail.com>
2018-04-25 09:47:26 +0200

    -moved camera functions from camera controller to extension file

--- oneshotchallenge/VCs/CameraController/CameraController.swift
+++ oneshotchallenge/VCs/CameraController/CameraController.swift
@@ -61,71 +61,7 @@ class CameraController: UIViewController, AVCapturePhotoCaptureDelegate {
         }
     }
     
-    fileprivate func setupCaptureSession() {
-        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
-        let captureSession = AVCaptureSession()
-        
-        do {
-            let input = try AVCaptureDeviceInput(device: captureDevice)
-            if captureSession.canAddInput(input) {
-                captureSession.addInput(input)
-            }
-        } catch let error {
-            debugPrint(error.localizedDescription)
-        }
-        
-        if captureSession.canAddOutput(output) {
-            captureSession.addOutput(output)
-        }
-        
-        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        previewLayer?.videoGravity = .resizeAspectFill
-        captureSession.startRunning()
-        
-        DispatchQueue.main.async {
-            self.previewLayer?.frame = self.capturePreviewView.bounds
-            guard let previewLayer = self.previewLayer else { return }
-            self.capturePreviewView.layer.addSublayer(previewLayer)
-        }
-    }
-    
-    @objc fileprivate func capturePhoto() {
-        let settings = AVCapturePhotoSettings()
-        
-        guard let previewFormatType = settings.availablePreviewPhotoPixelFormatTypes.first else { return }
-        settings.previewPhotoFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewFormatType]
-        DispatchQueue.global(qos: .default).async {
-            self.output.capturePhoto(with: settings, delegate: self)
-        }
-    }
-    
-    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-        guard let imageData = photo.fileDataRepresentation() else { return }
-        
-        guard let takenPhoto = UIImage(data: imageData) else { return }
-        guard let croppedPhoto = cropToPreviewLayer(originalImage: takenPhoto) else { return }
-        
-        DispatchQueue.main.async {
-            self.goToEditPhotoController(image: croppedPhoto)
-        }
-    }
-    
-    func cropToPreviewLayer(originalImage: UIImage) -> UIImage? {
-        guard let previewLayer = previewLayer else { return nil }
-        let bounds = CGRect(x: 0, y: 0, width: previewLayer.frame.size.width, height: previewLayer.frame.size.height)
-        let outputRect = previewLayer.metadataOutputRectConverted(fromLayerRect: bounds)
-        var cgImage = originalImage.cgImage!
-        let width = CGFloat(cgImage.width)
-        let height = CGFloat(cgImage.height)
-        let cropRect = CGRect(x: outputRect.origin.x * width, y: outputRect.origin.y * height, width: outputRect.size.width * width, height: outputRect.size.height * height)
-        
-        cgImage = cgImage.cropping(to: cropRect)!
-        let croppedUIImage = UIImage(cgImage: cgImage, scale: 1.0, orientation: originalImage.imageOrientation)
-        
-        return croppedUIImage
-    }
-    
-    fileprivate func goToEditPhotoController(image: UIImage) {
+    func goToEditPhotoController(image: UIImage) {
         let controller = EditPhototController()
         controller.photo = image
         
