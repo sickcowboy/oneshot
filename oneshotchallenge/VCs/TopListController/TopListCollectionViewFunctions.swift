a3bc4bc52e72b62dd6adc1a1b84d020aaddd5891
GalvenD <dennis.galven@gmail.com>
2018-05-14 14:28:56 +0200

    -added segmented control to navigation bar in top list controller -collection view animates when segmented control is pressed in top list controller

--- oneshotchallenge/VCs/TopListController/TopListCollectionViewFunctions.swift
+++ oneshotchallenge/VCs/TopListController/TopListCollectionViewFunctions.swift
@@ -10,29 +10,23 @@ import UIKit
 
 extension TopListController {
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 3
+        return 1
     }
     
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId,
                                                                      for: indexPath) as! TopListControllerHeader
-        if indexPath.section == 0 {
-            header.setTitle(title: "Today")
-        } else if indexPath.section == 1 {
-            header.setTitle(title: "Monthly")
-        } else {
-            header.setTitle(title: "Yearly")
-        }
-
+        let title = segmentedView.titleForSegment(at: segmentedView.selectedSegmentIndex)
+        header.setTitle(title: title ?? "")
         return header
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: collectionView.frame.width, height: 22)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 0, bottom: 8, right: 0)
+        return UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 0)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
--- oneshotchallenge/VCs/TopListController/TopListController.swift
+++ oneshotchallenge/VCs/TopListController/TopListController.swift
@@ -13,11 +13,18 @@ class TopListController: UICollectionViewController, UICollectionViewDelegateFlo
     let cellId = "cellId"
     let headerId = "headerId"
     
+    lazy var segmentedView : UISegmentedControl = {
+        let sV = UISegmentedControl(items: ["Today", "Monthly", "Yearly"])
+        sV.tintColor = Colors.sharedInstance.primaryTextColor
+        sV.selectedSegmentIndex = 0
+        sV.addTarget(self, action: #selector(segmentChanged(_:)), for: .valueChanged)
+        return sV
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        StausBar.sharedInstance.changeColor(view: view)
+        navigationItem.titleView = segmentedView
         
-        navigationController?.navigationBar.isHidden = true
+        view.backgroundColor = Colors.sharedInstance.primaryColor
         
         collectionView?.backgroundColor = Colors.sharedInstance.primaryColor
         
@@ -27,5 +34,19 @@ class TopListController: UICollectionViewController, UICollectionViewDelegateFlo
                                  withReuseIdentifier: headerId)
         
         (collectionViewLayout as! UICollectionViewFlowLayout).sectionHeadersPinToVisibleBounds = true
+    }
+    
+    @objc fileprivate func segmentChanged(_ sender: UISegmentedControl) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+            self.collectionView?.transform = CGAffineTransform(translationX: -self.view.frame.width, y: 0)
+        }) { (_) in
+            
+            self.collectionView?.transform = CGAffineTransform(translationX: self.view.frame.width, y: 0)
+            self.collectionView?.reloadData()
+            
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+                self.collectionView?.transform = CGAffineTransform(translationX: 0, y: 0)
+            }, completion: nil)
+        }
     }
 }
